3ce1f9d553dc21ad12938fc62c2995782bbdcfa3
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2023-12-30 23:02:11 +0530

    navbar created for profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -36,14 +36,6 @@ export default function ProfilePage() {
           <Link href={`/profile/${data}`}>{data}</Link>
         )}
       </h2>
-      <hr />
-
-      <button
-        onClick={logout}
-        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Logout
-      </button>
       <button
         onClick={getUserDetails}
         className="bg-purple-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
--- src/app/ui/Navbar.tsx
+++ src/app/ui/Navbar.tsx
@@ -1,11 +1,61 @@
 "use client";
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export const Navbar = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
   const pathname = usePathname();
-  console.log(pathname);
+
+  const getUser = async () => {
+    if (pathname === "/profile") {
+      const response = await axios.get("/api/users/me");
+      const username = response.data.data.username;
+      setUsername(username);
+    }
+  };
+  useEffect(() => {
+    getUser();
+  });
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+  if (pathname === "/profile") {
+    return (
+      <div className="flex justify-between border-b-2 drop-shadow-md border-slate-900  ">
+        <Link href={"/"}>
+          <Image
+            className="mx-2"
+            src="/logoimg.png"
+            alt="logo"
+            width={"60"}
+            height={"60"}
+          />
+        </Link>
+        <h2 className="flex items-center text-xl font-semibold ">{username}</h2>
+
+        <button
+          onClick={logout}
+          className="p-2 mt-2 mr-3 border-b-2 border-gray-300  bg-slate-900 rounded-lg mb-4 hover:border-blue-500 hover:bg-slate-800"
+        >
+          {"Logout"}
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="flex justify-between border-b-2 drop-shadow-md border-slate-900  ">
       <Link href={"/"}>
@@ -14,7 +64,7 @@ export const Navbar = () => {
           src="/logoimg.png"
           alt="logo"
           width={"60"}
-          height={"50"}
+          height={"60"}
         />
       </Link>
       <Link
